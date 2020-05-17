375f196557602ba38214609364b64ce2987a5259
Vasil Markov <vasilgmarkov@gmail.com>
2020-05-17 15:20:52 +0200

    add CRUD for author and book

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,49 @@
 class AuthorsController < ApplicationController
     def index
-
         authors = Author.all
         render json: authors, include: [:books, :genres]
-        
+    end
+
+    def show
+        render json: @uthor
+    end
+
+    def create
+        @author = Author.new (author_params)
+
+        if @author.save
+            render json: @author, status: :created
+        else
+            render json: @author.errors , status: :unprocessable_entity    
+        end
+    end
+
+    def update
+        if @author.update(author_params)
+            render json: @author
+        else
+            render json: @author.errors , status: :unprocessable_entity    
+        end
+    end
+    
+    def destroy
+        @author.destroy
+        if  @author.destroy
+            head :no_content, status: :ok
+        else
+            render json: @author.errors, status: :unprocessable_entity 
+        end      
+    end
+
+    private
+
+    # Only allow a trusted parameter "white list" through.
+    def author_params
+        params.require(:author).permit(:name)
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+        @author = Author.find(params[:id])
     end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,51 @@
 class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :update, :destroy]
     def index
         books = Book.all
         render json: books, include: [:author, :genre]  
+    end
+
+    def show
+        render json: @book
+    end
+
+    def create
+        @book = Book.new (book_params)
+
+        if @book.save
+            render json: @book, status: :created
+        else 
+            render json: @book.errors, status: :unprocessable_entity   
+        end
+    end
+
+    def update
+        if @book.update(book_params)
+            render json: @book
+        else
+            render jsnon: @book.errors, status: :unprocessable_entity 
+        end    
+    end
+
+    def destroy
+        @book.destroy
+        if  @book.destroy
+            head :no_content, status: :ok
+        else
+            render json: @book.errors, status: :unprocessable_entity 
+        end 
+    end
+
+    private
+
+    # Only allow a trusted parameter "white list" through.
+    def book_params
+        params.require(:book).permit(:title, :author_id, :genre_id)
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+        @book = Book.find(params[:id])
     end
     
 end
